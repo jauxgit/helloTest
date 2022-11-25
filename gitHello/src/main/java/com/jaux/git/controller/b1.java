7ba262c6caf602240eae27d2a1efc3e32926d3f5
jaux <jaux@santa.cn>
2022-11-25 15:48:10 +0800

    提交idea测试

--- gitHello/src/main/java/com/jaux/git/controller/b1.java
+++ gitHello/src/main/java/com/jaux/git/controller/b1.java
@@ -1,7 +1,7 @@
 package com.jaux.git.controller;
 
-public class Hello {
+public class b1 {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println("b1");
     }
 }
